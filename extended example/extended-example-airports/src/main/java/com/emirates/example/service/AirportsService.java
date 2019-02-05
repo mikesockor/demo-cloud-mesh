4b6ca71f26a5243941411ab47238a478893682e4
Mikhail Sokor <mikhail.sokor@dnata.com>
2019-02-05 09:39:18 +0400

    airport: imperative to stream api

--- extended example/extended-example-airports/src/main/java/com/emirates/example/service/AirportsService.java
+++ extended example/extended-example-airports/src/main/java/com/emirates/example/service/AirportsService.java
@@ -6,15 +6,14 @@ import com.emirates.example.model.Coordinates;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class AirportsService {
     private static Logger               logger   = Logger.getLogger(AirportsService.class.getName());
@@ -24,23 +23,23 @@ public class AirportsService {
         if (airports.isEmpty()) {
             InputStream inputStream = AirportsService.class.getResourceAsStream("/airports.csv");
             CSVReader reader = new CSVReader(new InputStreamReader(inputStream), '\t');
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                Airport airport = new Airport();
-                airport.setName(nextLine[0]);
-                airport.setCity(nextLine[1]);
-                airport.setCountry(nextLine[2]);
-                airport.setCode(nextLine[3]);
-                airport.setCoordinates(new Coordinates(nextLine[5], nextLine[6], nextLine[7]));
-                airport.setZoneId(nextLine[10]);
-                addAirport(airport);
-            }
+
+            airports = reader.readAll().stream()
+                .map(nextLine -> {
+                    Airport airport = new Airport();
+                    airport.setName(nextLine[0]);
+                    airport.setCity(nextLine[1]);
+                    airport.setCountry(nextLine[2]);
+                    airport.setCode(nextLine[3]);
+                    airport.setCoordinates(new Coordinates(nextLine[5], nextLine[6], nextLine[7]));
+                    airport.setZoneId(nextLine[10]);
+                    return airport;
+                })
+                .collect(Collectors.toMap(Airport::getCode, e -> e)
+                )
+            ;
             logger.info("Populated " + airports.size() + " airports");
         }
-    }
-
-    private static void addAirport(Airport airport) {
-        airports.put(airport.getCode(), airport);
     }
 
     public static Airport getAirport(String code) {
@@ -57,17 +56,10 @@ public class AirportsService {
         return Collections.unmodifiableSet(airports.keySet());
     }
 
-    public static Collection<Airport> filter(String filter) {
-        List<Airport> results = new ArrayList<>();
-        logger.info("Will filter airports for " + filter);
-        filter = filter.toUpperCase(Locale.US);
-        for (Airport airport : airports.values()) {
-            if (airport.getCity().toUpperCase(Locale.US).startsWith(filter)) {
-                results.add(airport);
-            }
-        }
-        results.sort(Airport.cityNameComparator);
-        logger.info("Returning " + results);
-        return results;
+    public static Collection<Airport> filter(final String filter) {
+        return airports.values().stream()
+            .filter(airport -> airport.getCity().toUpperCase(Locale.US).startsWith(filter.toUpperCase(Locale.US)))
+            .collect(Collectors.toList())
+            ;
     }
 }
